b798f0c545a6437cf5f3bd0b0e0b88dd29ff9a93
edsonalcala <edson_alcala@msn.com>
2024-07-16 19:53:33 +0100

    v0 working

--- src/block_streamer.rs
+++ src/block_streamer.rs
@@ -1,5 +1,10 @@
+use near_jsonrpc_client::errors::JsonRpcError;
+use near_jsonrpc_client::methods::block::RpcBlockError;
+use near_jsonrpc_client::methods::chunk::ChunkReference;
 use near_jsonrpc_client::{methods, JsonRpcClient};
+use near_primitives::hash::CryptoHash;
 use near_primitives::types::{BlockId, BlockReference, Finality};
+use near_primitives::views::{ActionView, BlockView, ChunkView};
 
 pub fn specify_block_reference(last_processed_block: u64) -> BlockReference {
     if last_processed_block == 0 {
@@ -12,20 +17,36 @@ pub fn specify_block_reference(last_processed_block: u64) -> BlockReference {
 pub async fn fetch_block(
     client: &JsonRpcClient,
     block_reference: BlockReference,
-) -> Result<near_primitives::views::BlockView, Box<dyn std::error::Error>> {
+) -> Result<BlockView, JsonRpcError<RpcBlockError>> {
     let block_request = methods::block::RpcBlockRequest { block_reference };
     let block_response = client.call(block_request).await;
+    block_response
+}
 
-    match block_response {
-        Ok(block_details) => {
-            println!("{:#?}", block_details);
-            Ok(block_details)
+pub async fn fetch_chunk(
+    client: &JsonRpcClient,
+    chunk_hash: CryptoHash,
+) -> Result<ChunkView, Box<dyn std::error::Error>> {
+    let chunk_reference = ChunkReference::ChunkHash {
+        chunk_id: chunk_hash,
+    };
+
+    let chunk_request = methods::chunk::RpcChunkRequest {
+        chunk_reference: chunk_reference,
+    };
+
+    let chunk_response = client.call(chunk_request).await;
+
+    match chunk_response {
+        Ok(chunk_details) => {
+            // println!("{:#?}", chunk_details);
+            Ok(chunk_details)
         }
         Err(err) => match err.handler_error() {
-            // Handle unknown block error
-            Some(methods::block::RpcBlockError::UnknownBlock { .. }) => {
-                println!("(i) Unknown block!");
-                panic!("Unknown block!");
+            // Handle unknown chunk error
+            Some(methods::chunk::RpcChunkError::UnknownChunk { .. }) => {
+                println!("(i) Unknown chunk!");
+                panic!("Unknown chunk!");
             }
             // Handle other handler errors
             Some(err) => {
@@ -39,4 +60,33 @@ pub async fn fetch_block(
             }
         },
     }
+}
+
+pub async fn find_transaction_in_block(
+    client: &JsonRpcClient,
+    block: &BlockView,
+    account_id: &str,
+    method_name: &str,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    for chunk_header in &block.chunks {
+        let chunk_hash = chunk_header.chunk_hash;
+        let chunk = fetch_chunk(client, chunk_hash).await?;
+
+        for transaction in &chunk.transactions {
+            if transaction.receiver_id == account_id {
+                for action in &transaction.actions {
+                    if let ActionView::FunctionCall {
+                        method_name: action_method_name,
+                        ..
+                    } = action
+                    {
+                        if action_method_name == method_name {
+                            return Ok(true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Ok(false)
 }
--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,6 @@
-// pub const ACCOUNT_TO_LISTEN: &str = "hassel.near"; TODO: Set proper account
+pub const ACCOUNT_TO_LISTEN: &str = "halcalag.testnet";
+pub const FUNCTION_TO_LISTEN: &str = "set_greeting";
 pub const DB_PATH: &str = "./data";
 pub const LAST_PROCESSED_BLOCK_KEY: &[u8] = b"last_processed_block";
+pub const NEAR_RPC_URL: &str = "https://rpc.testnet.near.org";
+// pub const ARCHIVAL_NEAR_RPC_URL: &str = "https://archival-rpc.testnet.near.org";
