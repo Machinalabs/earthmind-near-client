1b74998c8233ddd274eb283566ac5c1dc22bb109
Hassel Alcalá <hassel_aurora_13@hotmail.com>
2024-07-18 08:55:12 -0600

    add cli

--- src/block_streamer.rs
+++ src/block_streamer.rs
@@ -6,6 +6,10 @@ use near_primitives::hash::CryptoHash;
 use near_primitives::types::{BlockId, BlockReference, Finality};
 use near_primitives::views::{ActionView, BlockView, ChunkView};
 
+use crate::constants::*;
+use crate::database::{load_last_processed_block, save_last_processed_block};
+use std::sync::{Arc, Mutex};
+
 pub fn specify_block_reference(last_processed_block: u64) -> BlockReference {
     if last_processed_block == 0 {
         BlockReference::Finality(Finality::Final)
@@ -89,4 +93,62 @@ pub async fn find_transaction_in_block(
         }
     }
     Ok(false)
+}
+
+pub async fn run_mode<F>(
+    client: &JsonRpcClient,
+    db: &Arc<Mutex<rocksdb::DB>>,
+    process_transaction: F,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    F: Fn(&JsonRpcClient) -> Result<bool, Box<dyn std::error::Error>>
+        + Send
+        + Sync,
+{
+    loop {
+        let last_processed_block = load_last_processed_block(db)?;
+        println!("Last processed block: {}", last_processed_block);
+
+        let block_reference = specify_block_reference(last_processed_block);
+        match fetch_block(client, block_reference).await {
+            Ok(block) => {
+                println!("Processing block: {:#?}", block.header.height);
+
+                // Check if the block contains the transaction of interest
+                if find_transaction_in_block(&client, &block, ACCOUNT_TO_LISTEN, FUNCTION_TO_LISTEN)
+                    .await?
+                {
+                    println!("Found transaction in block: {}", block.header.height);
+                    // TODO: Implement your logic here to handle the found transaction
+                    let _ = process_transaction(client);
+                }
+
+                // Save the new block height as the last processed block
+                let new_block_height = block.header.height;
+                save_last_processed_block(db, new_block_height)?;
+                println!("Saved new block height: {}", new_block_height);
+            }
+            Err(err) => match err.handler_error() {
+                // Handle unknown block error
+                Some(methods::block::RpcBlockError::UnknownBlock { .. }) => {
+                    println!("(i) Unknown block!");
+                    // We skip the unknown block and save the block height of the unknown block as the last processed block
+                    let new_block_height = last_processed_block + 1;
+                    save_last_processed_block(&db, new_block_height)?;
+                    println!("Saved new block height: {}", new_block_height);
+                } // Handle other handled errors
+                Some(err) => {
+                    println!("(i) An error occurred `{:#?}`", err);
+                    panic!("Other error!");
+                }
+                // Non handled errors
+                _ => {
+                    println!("(i) A non-handler error occurred `{:#?}`", err);
+                    panic!("Non handled error!");
+                }
+            },
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,54 @@
-use near_jsonrpc_client::{methods, JsonRpcClient};
+use clap::Parser;
+//use near_jsonrpc_client::{methods, JsonRpcClient};
 use std::sync::{Arc, Mutex};
-use tokio::time::{sleep, Duration};
+//use tokio::time::{sleep, Duration};
 
 mod block_streamer;
+mod cli;
 mod constants;
 mod database;
 
-use block_streamer::{fetch_block, find_transaction_in_block, specify_block_reference};
-use constants::{ACCOUNT_TO_LISTEN, DB_PATH, FUNCTION_TO_LISTEN, NEAR_RPC_URL};
-use database::{init_db, load_last_processed_block, save_last_processed_block};
+//use block_streamer::{fetch_block, find_transaction_in_block, specify_block_reference};
+//use constants::{ACCOUNT_TO_LISTEN, DB_PATH, FUNCTION_TO_LISTEN, NEAR_RPC_URL};
+//use database::{init_db, load_last_processed_block, save_last_processed_block};
+use block_streamer::run_mode;
+use cli::{Cli, Modes};
+use constants::{DB_PATH, NEAR_RPC_URL};
+use database::init_db;
+use near_jsonrpc_client::JsonRpcClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+
     // Open RocksDB connection
     let db = Arc::new(Mutex::new(init_db(DB_PATH)?));
-
-    // Load the last processed block
-    let last_processed_block = load_last_processed_block(&db)?;
-    println!("Last processed block: {}", last_processed_block);
 
     // Connect to the RPC client
     let client = JsonRpcClient::connect(NEAR_RPC_URL);
 
-    loop {
-        // Load the last processed block
-        let last_processed_block = load_last_processed_block(&db)?;
-        println!("Last processed block: {}", last_processed_block);
-
-        // Determine the block reference to fetch
-        let block_reference = specify_block_reference(last_processed_block);
-
-        // Fetch the block
-        match fetch_block(&client, block_reference).await {
-            Ok(block) => {
-                println!("Processing block: {:#?}", block.header.height);
-
-                // Check if the block contains the transaction of interest
-                if find_transaction_in_block(&client, &block, ACCOUNT_TO_LISTEN, FUNCTION_TO_LISTEN)
-                    .await?
-                {
-                    println!("Found transaction in block: {}", block.header.height);
-                    // TODO: Implement your logic here to handle the found transaction
-                }
-
-                // Save the new block height as the last processed block
-                let new_block_height = block.header.height;
-                save_last_processed_block(&db, new_block_height)?;
-                println!("Saved new block height: {}", new_block_height);
-            }
-            Err(err) => match err.handler_error() {
-                // Handle unknown block error
-                Some(methods::block::RpcBlockError::UnknownBlock { .. }) => {
-                    println!("(i) Unknown block!");
-
-                    // We skip the unknown block and save the block height of the unknown block as the last processed block
-                    let new_block_height = last_processed_block + 1;
-                    save_last_processed_block(&db, new_block_height)?;
-                    println!("Saved new block height: {}", new_block_height);
-                }
-                // Handle other handled errors
-                Some(err) => {
-                    println!("(i) An error occurred `{:#?}`", err);
-                    panic!("Other error!");
-                }
-                // Non handled errors
-                _ => {
-                    println!("(i) A non-handler error occurred `{:#?}`", err);
-                    panic!("Non handled error!");
-                }
-            },
+    match cli.mode {
+        Modes::Miner => {
+            run_mode(&client, &db, process_miner_transaction).await?;
         }
-
-        // Sleep for a short duration before checking for the next block
-        sleep(Duration::from_secs(2)).await;
+        Modes::Validator => {
+            run_mode(&client, &db, process_validator_transaction).await?;
+        }
     }
+
+    Ok(())
+}
+
+fn process_miner_transaction(client: &JsonRpcClient) -> Result<bool, Box<dyn std::error::Error>> {
+    // Implement your miner-specific logic here
+    println!("Process as miner");
+    Ok(false)
+}
+
+fn process_validator_transaction(
+    client: &JsonRpcClient,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    // Implement your validator-specific logic here
+    println!("Process as validator");
+    Ok(false)
 }
